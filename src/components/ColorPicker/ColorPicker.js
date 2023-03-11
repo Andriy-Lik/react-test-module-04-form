bfc43b697418a0f6c826b4f2fd9ef9dba96da4fc
Andriy-Lik <alan321987@gmail.com>
2023-03-11 10:00:26 +0200

    зміна мови

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -13,7 +13,7 @@ export default function ColorPicker({ options }) {
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Color Picker</h2>
-            <p>Выбран цвет: {label}</p>
+            <p>Вибраний колір: {label}</p>
             <div>
                 {options.map(({ label, color }, index) => (
                     <button 
